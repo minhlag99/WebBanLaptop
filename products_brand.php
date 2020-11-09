ae4cf76e866b4f57c22461a72b3346294475421c
minhlag99 <66405955+minhlag99@users.noreply.github.com>
2020-11-09 19:03:17 +0700

    suadoi

--- products_brand.php
+++ products_brand.php
@@ -14,7 +14,6 @@
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
     <!-- Additional CSS Files -->
     <link rel="stylesheet" href="assets/css/fontawesome.css">
     <link rel="stylesheet" href="assets/css/style.css">
